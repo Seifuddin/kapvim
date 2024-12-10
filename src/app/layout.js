5a9205c855d269b418563faeb3ed5b4014dd46b9
unknown <nguruedwin46@gmail.com>
2024-12-11 01:17:21 +0300

    Initial commit

---
+++ src/app/layout.js
@@ -0,0 +1,33 @@
+import { Inter } from 'next/font/google'
+import localFont from "next/font/local";
+import Header from '@/components/Header';
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata = {
+  title: "Capvim website",
+  description: "This is the main website for Capvim International Publishers",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/components/MenuItem.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function MenuItem({ title, address}) {
+    return (
+      <div>
+        <Link href={address} className="hover:text-amber-500">
+          <p className="uppercase text-sm">{title}</p>
+        </Link>
+      </div>
+    );
+  }
